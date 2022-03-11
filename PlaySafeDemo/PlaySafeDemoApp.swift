9aeb76a4b95d443815f907ed3c66cf52656fd90b
Calvin Lau <calvinlau6@gmail.com>
2022-03-11 18:08:23 +0800

    Initial Commit

---
+++ PlaySafeDemo/PlaySafeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySafeDemoApp.swift
+//  PlaySafeDemo
+//
+//  Created by Calvin Lau on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlaySafeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
